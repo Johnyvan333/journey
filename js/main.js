efe1531506c009f1955fe68837fe30af16dc3544
Johnyvan333 <vanchez666@gmail.com>
2016-12-12 00:08:13 +0300

    готово без адаптива и слайдера

--- js/main.js
+++ js/main.js
@@ -5,10 +5,13 @@ pickmeup('.pickmeup');
 var upperLetter = document.querySelector('.text-main__letter');
 var letterInner = document.querySelector('.text-main__upperLetter');
 var dateInner = document.querySelector('.about__date');
+var aside = document.querySelector('.page-header__aside-text');
+var asideBtn = document.getElementById('asideBtn');
 
 function setLetter() {
   letterInner.innerHTML = upperLetter.innerHTML;
 }
+setLetter();
 
 function setDate() {
   var currentDate = new Date();
@@ -16,5 +19,16 @@ function setDate() {
   var currentYear = currentDate.getFullYear();
   dateInner.innerHTML += currentMonth + ', ' + currentYear;
 }
-setLetter();
 setDate();
+
+asideBtn.addEventListener('click', function(event) {
+  if (asideBtn.classList.contains('aside-btn-more')) {
+    asideBtn.classList.remove('aside-btn-more');
+    aside.classList.remove('aside-more');
+    asideBtn.innerHTML = 'more';
+  } else {
+    asideBtn.classList.add('aside-btn-more');
+    aside.classList.add('aside-more');
+    asideBtn.innerHTML = 'less';
+  }
+});
