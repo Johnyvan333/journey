92e2f16cf6671fe5c81afacaca509c7d579d1909
Johnyvan333 <vanchez666@gmail.com>
2016-12-11 21:10:20 +0300

    готово без адаптива и слайдера

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-pickmeup('#pickmeup');
+pickmeup('.pickmeup');
+
+var upperLetter = document.querySelector('.text-main__letter');
+var letterInner = document.querySelector('.text-main__upperLetter');
+var dateInner = document.querySelector('.about__date');
+
+function setLetter() {
+  letterInner.innerHTML = upperLetter.innerHTML;
+}
+
+function setDate() {
+  var currentDate = new Date();
+  var currentMonth = currentDate.toString().slice(4, 7);
+  var currentYear = currentDate.getFullYear();
+  dateInner.innerHTML += currentMonth + ', ' + currentYear;
+}
+setLetter();
+setDate();
